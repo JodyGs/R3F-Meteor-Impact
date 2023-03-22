17f9317ce038bfacf444eeb45accc438c1ace0ca
JodyGs <demajn@tuta.io>
2023-03-22 15:22:32 +0100

    scene, meteor, beams

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ import {
 	Environment,
 } from "@react-three/drei";
 import Scene from "./Scene";
+import { useThree } from "@react-three/fiber";
+import Meteor from "./Meteor";
+import { NodeToyTick } from "@nodetoy/react-nodetoy";
+import Beams from "./Beams";
 
 
 
 function App() {
+
+  const state = useThree();
+
+  React.useEffect(() => {
+    state.gl.toneMappingExposure = 5;
+  }, [state.gl])
+
 	return (
 		<>
 			<Environment
@@ -21,15 +32,19 @@ function App() {
 			<PerspectiveCamera
 				makeDefault
 				fov={33}
-				position={[-0.09, 16.01, -27.9]}
+				position={[-0.07, 16.41, -24.1]}
 			/>
 			<OrbitControls
-				target={[0.304, 0.806, 0.427]}
+				target={[0.02, 0.806, 0.427]}
 				maxPolarAngle={Math.PI * 0.45}
 			/>
 
+      <NodeToyTick />
+
       <Suspense fallBack={null}>
         <Scene />
+        <Meteor />
+        <Beams />
       </Suspense>
 		</>
 	);
